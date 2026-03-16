3ca4f35a347616db0e3013a1a2238124db6a34aa
Mitya <dmitriy.akhmediyev@gmail.com>
2026-03-16 15:44:23 +0300

    main pipeline of work with architecture generation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from verifier.verifier import verify_architecture
 from llm import LLMClient, generate_architecture
 from system_prompt import SYSTEM_PROMPT
 from prompts.code_generation_prompts import PYTHON_PROMPT
+from prompts.rebuild_arch_prompts import build_repair_prompt
 
 
 llm = LLMClient()
@@ -18,6 +19,9 @@ if errors:
     print("Verification failed")
     for e in errors:
         print(e)
+    prompt_edited = build_repair_prompt(SYSTEM_PROMPT, errors)
+    # re-generate arch
+    llm.generate(prompt_edited, USER_PROMPT)
 else:
     print("Architecture is valid")
     llm.generate_code_from_architecture("sessions/architecture.yaml", 
